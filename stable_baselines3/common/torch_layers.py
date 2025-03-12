8661201dbde207645e3725fdcd8c84caa38dde53
justyna3773 <60304923+justyna3773@users.noreply.github.com>
2025-03-12 22:13:00 +0100

    Update torch_layers.py, added attention to CNN

--- stable_baselines3/common/torch_layers.py
+++ stable_baselines3/common/torch_layers.py
@@ -44,9 +44,95 @@ class FlattenExtractor(BaseFeaturesExtractor):
 
     def forward(self, observations: th.Tensor) -> th.Tensor:
         return self.flatten(observations)
-
-
 class NatureCNN(BaseFeaturesExtractor):
+    """
+    CNN from DQN Nature paper:
+        Mnih, Volodymyr, et al.
+        "Human-level control through deep reinforcement learning."
+        Nature 518.7540 (2015): 529-533.
+ 
+    :param observation_space: The observation space of the environment
+    :param features_dim: Number of features extracted.
+        This corresponds to the number of unit for the last layer.
+    :param normalized_image: Whether to assume that the image is already normalized
+        or not (this disables dtype and bounds checks): when True, it only checks that
+        the space is a Box and has 3 dimensions.
+        Otherwise, it checks that it has expected dtype (uint8) and bounds (values in [0, 255]).
+    """
+ 
+    def __init__(
+        self,
+        observation_space: gym.Space,
+        features_dim: int = 512,
+        normalized_image: bool = False,
+    ) -> None:
+        assert isinstance(observation_space, spaces.Box), (
+            "NatureCNN must be used with a gym.spaces.Box ",
+            f"observation space, not {observation_space}",
+        )
+        super().__init__(observation_space, features_dim)
+        # We assume CxHxW images (channels first)
+        # Re-ordering will be done by pre-preprocessing or wrapper
+        assert is_image_space(observation_space, check_channels=False, normalized_image=normalized_image), (
+            "You should use NatureCNN "
+            f"only with images not with {observation_space}\n"
+            "(you are probably using `CnnPolicy` instead of `MlpPolicy` or `MultiInputPolicy`)\n"
+            "If you are using a custom environment,\n"
+            "please check it using our env checker:\n"
+            "https://stable-baselines3.readthedocs.io/en/master/common/env_checker.html.\n"
+            "If you are using `VecNormalize` or already normalized channel-first images "
+            "you should pass `normalize_images=False`: \n"
+            "https://stable-baselines3.readthedocs.io/en/master/guide/custom_env.html"
+        )
+        n_input_channels = observation_space.shape[0]
+        self.conv1 = nn.Conv2d(n_input_channels, 32, kernel_size=8, stride=4, padding=0)
+        self.norm = nn.LayerNorm(32)
+        self.mha = nn.MultiheadAttention(32,num_heads=1, batch_first=True)
+        self.scale = nn.Parameter(th.zeros(1))
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0)
+        self.bn1 = nn.BatchNorm2d(64)
+       
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.do = nn.Dropout(0.5)
+        n_flatten=3136
+        self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.ReLU())
+       
+        #self.fc_out = nn.Linear(64*4*4, 10)
+        # self.cnn = nn.Sequential(
+        #     nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
+        #     nn.ReLU(),
+        #     nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
+        #     nn.ReLU(),
+        #     nn.Flatten(),
+        # )
+ 
+        # Compute shape by doing one forward pass
+        # with th.no_grad():
+        #     n_flatten = self.forward(th.as_tensor(observation_space.sample()[None]).float()).shape[1]
+ 
+        #self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.ReLU())
+    def use_attention(self, x):
+        bs, c, h, w = x.shape #4, 32, 20, 20
+        x_att = x.reshape(bs, c, h*w).transpose(1, 2) # 4, 400, 42
+        x_att = self.norm(x_att)
+        att_out, att_map = self.mha(x_att, x_att, x_att)
+        return att_out.transpose(1,2).reshape(bs, c, h, w), att_map
+   
+    def forward(self, observations: th.Tensor) -> th.Tensor:
+        x = self.conv1(observations)#4,32,20,20
+        #there is a problem in this call below
+        x = self.scale*self.use_attention(x)[0] + x
+        x = self.relu(x)
+        x =self.relu(self.bn1(self.conv2(x))) #4,64,9,9
+        x =self.relu(self.bn2(self.conv3(x))) #4, 64, 7, 7
+        x = self.do(x.reshape(x.shape[0], -1))#4, 3136
+        #x = self.fc_out(x)
+        x = self.linear(x)
+        return x
+
+class NatureCNN_backup(BaseFeaturesExtractor):
     """
     CNN from DQN Nature paper:
         Mnih, Volodymyr, et al.
